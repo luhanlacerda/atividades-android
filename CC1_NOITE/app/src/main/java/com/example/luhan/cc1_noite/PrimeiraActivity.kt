2a0f6f9e94f1e5f462553c0ba8605aafb154e93e
Luhan Lacerda <luhanlacerda@gmail.com>
2018-02-20 20:37:46 -0300

    Primeira Atividade

---
+++ CC1_NOITE/app/src/main/java/com/example/luhan/cc1_noite/PrimeiraActivity.kt
@@ -0,0 +1,12 @@
+package com.example.luhan.cc1_noite
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PrimeiraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_primeira)
+    }
+}
